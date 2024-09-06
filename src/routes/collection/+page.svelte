9f537e89b86a7fcf2319f07ceff87e955ff0f262
tbollen <thomas.bollen97@gmail.com>
2024-09-06 19:05:53 +0200

    Added basic card overview

--- src/routes/collection/+page.svelte
+++ src/routes/collection/+page.svelte
@@ -1,11 +1,79 @@
 <!-- Collection -->
 <script lang="ts">
 	import Navbar from '$lib/components/Navbar.svelte';
+	import Gamecard from '$lib/components/Gamecard.svelte';
+	import GamecardBack from '$lib/components/GamecardBack.svelte';
+
+	import { items } from '$lib/stores/Items';
+	$: cards = items.items;
 </script>
 
 <main>
-	<section>
+	<section id="header">
 		<Navbar />
-		<h1 class="text-3xl font-bold underline">Collection</h1>
+	</section>
+	<section id="controls">Controls</section>
+	<section id="viewer">
+		{#each cards as card}
+			<div class="cardInViewer" class:isSelected={card.isSelected} id={card.id}>
+				<div class="frontSideCard">
+					<Gamecard item={card} />
+				</div>
+				<div class="backSideCard">
+					<GamecardBack item={card} />
+				</div>
+			</div>
+		{/each}
 	</section>
 </main>
+
+<style>
+	.cardInViewer {
+		/* Positioning */
+		position: relative;
+		/* Scaling down */
+		height: fit-content;
+		width: fit-content;
+		transform: scale(0.8);
+		margin: 5mm;
+		/* Click */
+		cursor: pointer;
+	}
+
+	.frontSideCard {
+		/* Positioning */
+		position: relative;
+		transition: all 0.4s ease-in-out;
+		box-sizing: content-box;
+	}
+
+	.backSideCard {
+		/* Positioning */
+		position: absolute;
+		top: 0%;
+		left: 0%;
+		z-index: -1;
+		transition: all 0.4s ease-in-out;
+	}
+
+	.cardInViewer:hover .frontSideCard {
+		box-shadow: 10px 10px 15px var(--color-text-1);
+		transform: rotate(-5deg);
+	}
+
+	.cardInViewer:hover .backSideCard {
+		left: 60%;
+		transform: rotate(5deg);
+		box-shadow: 10px 10px 15px var(--color-text-3);
+	}
+
+	.cardInViewer.isSelected .frontSideCard::after,
+	.cardInViewer.isSelected .backSideCard::after {
+		content: '';
+		position: absolute;
+		inset: -5px;
+		background-color: var(--color-blossom-2);
+		opacity: 1;
+		z-index: -1;
+	}
+</style>
