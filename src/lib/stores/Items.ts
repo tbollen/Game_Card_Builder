661357090caa09056f1036e269f58746b9771a05
tbollen <thomas.bollen97@gmail.com>
2024-09-07 14:15:24 +0200

    Major overhaul for functions of Itemstore

--- src/lib/stores/Items.ts
+++ src/lib/stores/Items.ts
@@ -58,6 +58,48 @@ class StoredItem extends Item {
 		// Set Date Created to now
 		this.dateCreated = _storedItem?.dateCreated || new Date();
 	}
+
+	// Methods for adding new fields and editing
+	addEmptyField(fieldType: FieldTypes) {
+		const emptyField = { name: fieldType, description: 'text' };
+		let _fields = this[fieldType];
+		if (_fields) {
+			// If there are already fields, push
+			_fields.push(emptyField);
+		} else {
+			// Otherwise, create a new array
+			_fields = [emptyField];
+		}
+		// Update to match
+		this[fieldType] = _fields;
+	}
+
+	removeField(fieldType: FieldTypes, index: number) {
+		let _fields = this[fieldType];
+		if (!_fields) throw new Error(`Field type ${fieldType} not found in ${this.name} (${this.id})`);
+		// If there are already fields, remove
+		_fields.splice(index, 1);
+		// Update to match
+		this[fieldType] = _fields;
+		this.update();
+	}
+
+	// Image editing
+	resetImagePosition(property?: 'x_offset' | 'y_offset' | 'rotation' | 'scale') {
+		if (this.image == undefined) return;
+		if (property) this.image[property] = property == 'scale' ? 100 : 0;
+		this.image.x_offset = 0;
+		this.image.y_offset = 0;
+		this.image.rotation = 0;
+		this.image.scale = 100;
+		this.update();
+	}
+
+	update() {
+		// Trigger reactivity
+		const _storedItem = this;
+		Object.assign(_storedItem, this);
+	}
 }
 class ItemStore {
 	items: StoredItem[] = [];
@@ -78,9 +120,45 @@ class ItemStore {
 		this.idSet = new Set(this.items.map((item) => item.id));
 		this.activeItem = this.getActiveItem();
 	}
+	// Basic Methods
+
+	getItem(_target: string | StoredItem): StoredItem {
+		if (_target instanceof StoredItem) return _target;
+		const _item = this.items.find((item) => item.id === _target);
+		if (!_item) throw new Error(`Item with id: ${_target} not found in items`);
+		return _item;
+	}
+
+	getFirstItem(): StoredItem {
+		if (this.items.length == 0) this.addNewItem();
+		return this.items[0];
+	}
+
+	getItemId(_item: StoredItem): string {
+		if (!this.items.includes(_item)) throw new Error(`Given Item not found in items`);
+		return _item.id;
+	}
+
+	setItem(_id: string, itemUpdate: Partial<Item>) {
+		// Get the items and update	the selected item
+		let _items = this.items;
+		_items = _items.map((item) => {
+			if (item.id === _id) {
+				return { ...item, ...itemUpdate } as StoredItem; //Override given properties from itemUpdate
+			} else {
+				return item;
+			}
+		});
+		// Update the itemstore
+		this.items = _items;
+	}
+
+	//
+	// DEPRECATED
+	//
 
 	// Item Management
-	addNewItem(_item?: Item) {
+	addNewItem(_item?: Partial<StoredItem>) {
 		let _items = this.items;
 		let _idSet = this.idSet;
 		const newItemId = this.generateUniqueId();
@@ -98,110 +176,69 @@ class ItemStore {
 		console.log(`New${_item ? '' : ' empty'} item added to database:`, newItem);
 	}
 
-	duplicateItem(_id: string) {
-		const _item = this.getItem(_id);
-		this.addNewItem(_item);
+	duplicateItem(_base: string | StoredItem) {
+		const _item = this.getItem(_base);
+		// Extract only the properties that should be duplicated
+		const _newItem = { ..._item };
+		this.addNewItem(_newItem);
 	}
 
 	destroy(_id: string) {
 		let _idSet = this.idSet;
 		let _items = this.items;
-		if (!_items.find((item) => item.id === _id))
-			return console.error(`Item with id: ${_id} not found in items`);
-		if (!_idSet.has(_id)) return console.error(`ID (${_id}) not found in idSet`);
-		const itemName = this.itemGetProperty('name', _id);
-		// Ask for confirmation
-		const confirmed = window.confirm(
-			`Are you sure you want to delete the card "${itemName}?" (id: ${_id})`
-		);
-		if (!confirmed) return;
-		// Remove from items
-		_items = _items.filter((item) => item.id !== _id);
-		_idSet.delete(_id);
-		// Update Items
-		this.items = _items;
-		this.idSet = _idSet;
-	}
-
-	getItem(_id?: string): StoredItem {
-		return this.items.find((item) => item.id === _id) || this.items[0];
-	}
-
-	setItem(_id: string, updates: StoredItem) {
-		const _item = this.items.find((item) => item.id === _id);
-		if (!_item) return console.error(`Item with id: ${_id} not found in items`);
-		Object.assign(_item, updates);
-		this.items = [...this.items];
-		// Update items
-		this.updateItems();
+		if (this.items.length < 2) {
+			alert('Cannot destroy last item');
+			throw new Error('Cannot destroy last item');
+		}
+		try {
+			// Remove item from Items
+			const _targetItem = this.getItem(_id);
+			const confirmed = window.confirm(
+				`Are you sure you want to delete the card "${_targetItem.name}?" (id: ${_id})`
+			);
+			// If confirmed, remove item
+			if (!confirmed) return;
+			_items = _items.filter((item) => item.id !== _targetItem.id);
+			// Update idSet
+			if (!_idSet.has(_id)) return console.error(`ID (${_id}) not found in idSet`);
+			_idSet.delete(_id);
+			// Update Items
+			this.items = _items;
+			this.idSet = _idSet;
+		} catch (error) {
+			// If item not found, re-throw error
+			console.error(error);
+		}
 	}
 
 	// Active Item Stuff
 	getActiveItem(): StoredItem {
-		let _activeItem = this.items.find((item) => item.isActive) as StoredItem;
-		if (!_activeItem) _activeItem = this.items[0];
-		return _activeItem;
+		if (!this.activeItem) this.activeItem = this.items[0];
+		return this.activeItem;
 	}
 
-	setActiveItem(id: string) {
-		this.items.forEach((item) => {
-			if (item.id === id) {
-				item.isActive = true;
-			} else {
-				item.isActive = false;
-			}
-		});
+	setActiveItem(_target: string | StoredItem) {
+		if (_target instanceof StoredItem) this.activeItem = _target;
+		else this.activeItem = this.getItem(_target);
 	}
 
 	// Item editing
-	itemGetProperty(property: keyof Item, id?: string) {
-		let _item = this.getActiveItem();
-		if (id) _item = this.items.find((item) => item.id === id) as StoredItem;
-		return _item[property];
-	}
-
-	itemSet(updates: Partial<Item>) {
-		Object.assign(this.getActiveItem(), updates);
-		this.activeItem = this.getActiveItem();
-		this.items = [...this.items];
-		// Update items
-		this.updateItems();
-	}
-
 	itemSetSuper(updates: Partial<StoredItem>) {
 		Object.assign(this.getActiveItem(), updates);
 	}
 
 	// Field editing
-	addEmptyField(fieldType: FieldTypes = 'fields') {
-		const _item = this.getActiveItem();
-		const emptyField = { name: fieldType, description: 'text' };
-		if (_item[fieldType]) _item[fieldType].push(emptyField);
-		else _item[fieldType] = [emptyField];
-		this.itemSet({ [fieldType]: _item[fieldType] });
-		this.updateItems();
+	addEmptyField(_target: string | StoredItem, fieldType: FieldTypes = 'fields') {
+		let _item = this.getItem(_target);
+		_item.addEmptyField(fieldType);
+		this.setItem(_item.id, _item);
 	}
 
-	removeField(fieldType: FieldTypes = 'fields', index: number) {
-		const _item = this.getActiveItem();
-		if (_item == undefined || _item[fieldType] == undefined) return;
-		if (index > _item[fieldType].length - 1) return;
-		if (_item[fieldType]) _item[fieldType].splice(index, 1);
-		this.itemSet({ [fieldType]: _item[fieldType] });
+	removeField(_target: string | StoredItem, fieldType: FieldTypes = 'fields', index: number) {
+		let _item = this.getItem(_target);
+		_item.removeField(fieldType, index);
+		this.setItem(_item.id, _item);
 	}
-
-	// Image editing
-	resetImagePosition(property?: 'x_offset' | 'y_offset' | 'rotation' | 'scale') {
-		const _item = this.getActiveItem();
-		if (_item.image == undefined) return;
-		if (property) _item.image[property] = property == 'scale' ? 100 : 0;
-		_item.image.x_offset = 0;
-		_item.image.y_offset = 0;
-		_item.image.rotation = 0;
-		_item.image.scale = 100;
-		this.itemSet({ image: _item.image });
-	}
-
 	// DB Editing
 
 	// ID Stuff
@@ -213,12 +250,8 @@ class ItemStore {
 		return _itemId;
 	}
 
-	private idIsUnique(_id: string): boolean {
-		return !this.idSet.has(_id);
-	}
-
 	private updateItems() {
-		editItem.update(() => this.getActiveItem());
+		// editItem.update(() => this.getActiveItem());
 		this.items = [...this.items];
 	}
 
@@ -246,5 +279,6 @@ if (typeof window !== 'undefined' && window.localStorage) {
 
 // Init store
 // export let items = new ItemStore();
-export let items = localStoreItems instanceof ItemStore ? localStoreItems : new ItemStore();
-export let editItem = writable<StoredItem>(items.getItem());
+// export let items = localStoreItems instanceof ItemStore ? localStoreItems : new ItemStore();
+export let items = new ItemStore();
+export let editItem = writable<StoredItem>(items.getFirstItem());
