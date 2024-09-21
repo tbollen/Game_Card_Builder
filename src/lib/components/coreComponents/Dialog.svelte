6351a35f8d633572f1c03992807a71bd3ba9444a
tbollen <thomas.bollen97@gmail.com>
2024-09-21 11:46:48 +0200

    Dialog skeleton

---
+++ src/lib/components/coreComponents/Dialog.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+
+	type Option = {
+		name: string;
+		onClick?: () => void;
+		autofocus?: boolean;
+	};
+
+	// Props
+	export let show: boolean;
+	export let close: () => void = () => {};
+	export let type: 'info' | 'warning' | 'error' | 'options' | 'prompt' = 'info';
+	export let response: any = undefined;
+	export let options: Option[] = [{ name: 'Ok', autofocus: true }, { name: 'Cancel' }];
+
+	let dialog: HTMLDialogElement;
+	$: if (dialog && show) dialog.showModal();
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (show = false)}
+	on:click|self={() => dialog.close()}
+	transition:fly
+>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div on:click|stopPropagation>
+		<slot name="header" />
+		<hr />
+		<slot />
+		<hr />
+		<!-- svelte-ignore a11y-autofocus -->
+		<button autofocus on:click={() => dialog.close()}>close modal</button>
+	</div>
+</dialog>
+
+<style>
+	dialog {
+		max-width: 32em;
+		border-radius: 0.2em;
+		border: none;
+		padding: 0;
+	}
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.3);
+	}
+	dialog > div {
+		padding: 1em;
+	}
+	dialog[open] {
+		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
+	@keyframes zoom {
+		from {
+			transform: scale(0.95);
+		}
+		to {
+			transform: scale(1);
+		}
+	}
+	dialog[open]::backdrop {
+		animation: fade 0.2s ease-out;
+	}
+	@keyframes fade {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+	button {
+		display: block;
+	}
+</style>
