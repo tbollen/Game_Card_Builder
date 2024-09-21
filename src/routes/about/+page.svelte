09ad049188e8757b2b3da0eff35f68bb245db31e
tbollen <thomas.bollen97@gmail.com>
2024-09-21 11:46:54 +0200

    test page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,16 @@
 <!-- About -->
 <script lang="ts">
 	import Navbar from '$lib/components/Navbar.svelte';
+
+	// Test with modals
+	import Dialog from '$lib/components/coreComponents/Dialog.svelte';
+	import Button from '$lib/components/coreComponents/Button.svelte';
+	let showDialog = false;
 </script>
 
 <Navbar />
 
 <h1 class="text-3xl font-bold underline">About this app</h1>
+
+<button on:click={() => (showDialog = true)}> show modal </button>
+<Dialog bind:show={showDialog} />
